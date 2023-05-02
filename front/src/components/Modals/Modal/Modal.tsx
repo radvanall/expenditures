1391bf7666341e0ab50d0449bcd253fdf92aab22
Radvan Alexandru <rip777ton@gmail.com>
2023-05-02 22:14:03 +0300

    Add the Modals folder to the project

---
+++ front/src/components/Modals/Modal/Modal.tsx
@@ -0,0 +1,37 @@
+import React, { FC, ReactNode } from "react";
+import styles from "./Modal.module.css";
+
+interface Props {
+  visible: boolean;
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  children?: ReactNode;
+}
+const Modal: FC<Props> = ({ visible, setVisible, children }) => {
+  return (
+    <div
+      className={
+        visible
+          ? `${styles.modal__wrapper} ${styles.active}`
+          : styles.modal__wrapper
+      }
+    >
+      <div
+        className={
+          visible
+            ? `${styles.modal__content} ${styles.active}`
+            : styles.modal__content
+        }
+      >
+        <div className={styles.btn__wrapper}>
+          <button className={styles.btn} onClick={() => setVisible(false)}>
+            {" "}
+            &#10005;
+          </button>
+        </div>
+        <div className={styles.children__wrapper}>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
