bbfedc1604a92534e9c016219c832d5d8b541043
Radvan Alexandru <rip777ton@gmail.com>
2023-05-06 21:01:13 +0300

    Change the basic Input to recive a label props

--- front/src/components/Inputs/BasicInput/BasicInput.tsx
+++ front/src/components/Inputs/BasicInput/BasicInput.tsx
@@ -5,20 +5,33 @@ import InputProps from "../../../Interfaces/InputProps";
 
 const BasicInput: FC<InputProps> = ({
   children,
+  value,
   width,
   type,
+  label,
   name,
+  borderColor,
   onBlur,
   onChange,
   inputRef,
 }) => {
+  console.log("bi", value);
   return (
-    <div style={{ width: width ?? "100%" }} className={styles.wrapper}>
+    <div
+      style={{ width: width ?? "100%" }}
+      className={
+        borderColor === "pink"
+          ? `${styles.wrapper} ${styles.pink}`
+          : styles.wrapper
+      }
+    >
       {children && <div className={styles.icon_container}>{children}</div>}
       <Input
         styles={styles}
+        value={value}
         name={name}
         type={type}
+        label={label}
         onBlur={onBlur}
         onChange={onChange}
         inputRef={inputRef}
@@ -26,5 +39,4 @@ const BasicInput: FC<InputProps> = ({
     </div>
   );
 };
-
 export default BasicInput;
