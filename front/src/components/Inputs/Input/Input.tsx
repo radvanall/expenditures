56c87ae684b7cfb40086312724578e8b94772389
Radvan Alexandru <rip777ton@gmail.com>
2023-05-06 20:56:35 +0300

    Add the label props to send the name of label. Before this the name of label was set to the same name as input.

--- front/src/components/Inputs/Input/Input.tsx
+++ front/src/components/Inputs/Input/Input.tsx
@@ -3,31 +3,38 @@ type Props = {
   styles: { [key: string]: string };
   name: string;
   type: string;
-  onBlur: (event: React.FocusEvent<HTMLInputElement>) => void;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  inputRef: React.Ref<HTMLInputElement>;
+  label: string;
+  value?: string | number;
+  onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  inputRef?: React.Ref<HTMLInputElement>;
 };
 const Input: FC<Props> = ({
   styles,
+  value,
   name,
+  label,
   onBlur,
   onChange,
   inputRef,
   type,
 }) => {
+  console.log("i", value);
   return (
     <div className={styles.wrapper}>
       <input
+        defaultValue={value}
+        // value={value}
         type={type}
         placeholder="any"
         name={name}
         onBlur={onBlur}
         onChange={onChange}
         ref={inputRef}
+        autoComplete="on"
       />
-      <label htmlFor="input">{name}</label>
+      <label htmlFor="input">{label}</label>
     </div>
   );
 };
-
 export default Input;
