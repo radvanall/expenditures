c4ddf8fa0355eb9962b2697083ed4807bd3f5cab
Radvan Alexandru <rip777ton@gmail.com>
2023-05-08 21:19:04 +0300

    Add the hook useMountTransition.tsx that  handles the logic of creating a transition effect for rendered elements

---
+++ front/src/services/hooks/useMountTransition.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export const useMountTransition = (isMouted: boolean, unmountDelay: number) => {
+  const [hasTransitionedIn, setHasTransitionedIn] = useState(false);
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout;
+    if (isMouted && !hasTransitionedIn) {
+      setHasTransitionedIn(true);
+    } else if (!isMouted && hasTransitionedIn) {
+      timeoutId = setTimeout(() => {
+        setHasTransitionedIn(false);
+      }, unmountDelay);
+    }
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [isMouted, hasTransitionedIn, unmountDelay]);
+  return hasTransitionedIn;
+};
