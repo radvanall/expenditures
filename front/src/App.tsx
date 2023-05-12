a00f9a35e1dd45d9463c2c43566445ab9382731c
Radvan Alexandru <rip777ton@gmail.com>
2023-05-12 21:49:59 +0300

    Add new  components in the App.tsx. RequireAuth and RequireUnauth wrapper components are used to allow or deny the access to some routes to authorized and unauthorized users

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,51 +1,33 @@
 import React from "react";
 import Layout from "./components/Layout/Layout";
 import { Routes, Route } from "react-router-dom";
-// interface formData{
-//   data:string;
-// }
-
+import RequireAuth from "./pages/ProtectedRoutesWrapper/RequireAuth";
 import "./App.css";
 import Login from "./pages/Login/Login";
 import About from "./pages/About";
+import Missing from "./pages/Missing/Missing";
+import Home from "./pages/Home/Home";
+import NewInvoice from "./pages/NewInvoice/NewInvoice";
+import RequireUnauth from "./pages/ProtectedRoutesWrapper/RequireUnauth";
 function App() {
-  // const login: React.FormEventHandler<HTMLFormElement> = (event) => {
-  //   event.preventDefault();
-  //   const data = new FormData(event.currentTarget);
-  //   const formData = new FormData();
-  //   const formObject: Record<string, string> = {};
-  //   for (const [key, value] of Object.entries(Object.fromEntries(data))) {
-  //     formObject[key] = value as string;
-  //   }
-
-  //   formData.append("request", "login");
-  //   formData.append("formData", JSON.stringify(formObject));
-  //   fetch("http://localhost:84/expenditures/public/userController.php", {
-  //     method: "POST",
-  //     headers: {
-  //     },
-  //     body: formData,
-  //   })
-  //     .then((res) => res.text())
-  //     .then((res) => console.log(res));
-  // };
-
+  console.log("app rerender");
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Login />} />
-          <Route path="about" element={<About />} />
+          <Route element={<RequireUnauth />}>
+            <Route path="login" element={<Login />} />
+            <Route path="about" element={<About />} />
+          </Route>
+
+          <Route element={<RequireAuth />}>
+            <Route path="home" element={<Home />} />
+            <Route path="new_invoice" element={<NewInvoice />} />
+          </Route>
+
+          <Route path="*" element={<Missing />} />
         </Route>
       </Routes>
-
-      {/* <form onSubmit={login}>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-        <button type="submit">Login</button>
-      </form> */}
     </div>
   );
 }
