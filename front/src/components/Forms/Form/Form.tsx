73c51f481838c319571b2be3f2ede5cd2bc86d87
Radvan Alexandru <rip777ton@gmail.com>
2023-05-12 22:06:36 +0300

    In Form.tsx Changed the property of FormData to a union type string | number; Added the ref and modal optional props in the Form component interface and in it’s props Added optional styling to the form’s parent div; Added value,label and border color props to the Input element inside Form element Made item.icon element optional In Form.module.css Changed the static values of color  to  css variables Add the form__container.modal class that is used when  the form’s parent component is a modal

--- front/src/components/Forms/Form/Form.tsx
+++ front/src/components/Forms/Form/Form.tsx
@@ -3,11 +3,13 @@ import styles from "./Form.module.css";
 import InputProps from "../../../Interfaces/InputProps";
 import { UseFormRegister, FieldErrors } from "react-hook-form";
 import { InputType } from "../../../Interfaces/InputType";
-type FormData = Record<string, string>;
+type FormData = Record<string, string | number>;
 
 interface Props {
   children?: ReactNode;
+  ref?: React.RefObject<HTMLFormElement>;
   formName: string;
+  modal?: boolean;
   Input: React.FC<InputProps>;
   inputFields: InputType[];
   register: UseFormRegister<FormData>;
@@ -18,29 +20,42 @@ interface Props {
 }
 const Form: FC<Props> = ({
   children,
+  ref,
   formName,
   Input,
   inputFields,
   register,
   submit,
   errors,
+  modal = false,
 }) => {
+  console.log("form", inputFields);
   return (
-    <div className={styles.form__container}>
+    <div
+      className={
+        modal
+          ? `${styles.form__container} ${styles.modal}`
+          : styles.form__container
+      }
+    >
       <h2>{formName}</h2>
-      <form onSubmit={submit} noValidate>
+      <form onSubmit={submit} noValidate ref={ref}>
         {inputFields.map((item) => {
           const reg = register(item.name);
           return (
             <div className={styles.input_container} key={item.id}>
               <Input
+                value={item.value}
+                key={item.key}
                 type={item.type}
+                label={item?.label ?? ""}
                 name={reg.name}
                 onChange={reg.onChange}
                 onBlur={reg.onBlur}
                 inputRef={reg.ref}
+                borderColor={item.border}
               >
-                <item.icon />
+                {item.icon && <item.icon />}
               </Input>
               {errors[item.name] && (
                 <span className={styles.error__message}>
