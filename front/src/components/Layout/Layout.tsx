47819839fdc32986e3a2d1f375f257911e130858
Radvan Alexandru <rip777ton@gmail.com>
2023-05-12 22:09:42 +0300

    Erased some unused  imports

--- front/src/components/Layout/Layout.tsx
+++ front/src/components/Layout/Layout.tsx
@@ -2,7 +2,6 @@ import React, { FC, useContext } from "react";
 // import styles from "./Layout.module.css";
 import { links } from "../../services/links";
 import { Outlet, Link, useLocation } from "react-router-dom";
-import useLogout from "../../services/hooks/useLogout";
 import Navbar from "../Navbar/Navbar";
 
 const Layout: FC = () => {
