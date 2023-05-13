80cfdd495b931ce5272527e17dffb0f16a26d535
Radvan Alexandru <rip777ton@gmail.com>
2023-05-13 23:16:46 +0300

    Add the forms to the Login page and set the .login class to display flex

--- front/src/pages/Login/Login.tsx
+++ front/src/pages/Login/Login.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import styles from "./Login.module.css";
+import RegistrationForm from "../../components/Forms/RegistrationForm/RegistrationForm";
+import LoginForm from "../../components/Forms/LoginForm/LoginForm";
+import { useState } from "react";
 
 const Login = () => {
-  return <div className={styles.login}></div>;
+  const [form, setForm] = useState<boolean>(true);
+  return (
+    <div className={styles.login}>
+      {form ? (
+        <LoginForm setForm={setForm} />
+      ) : (
+        <RegistrationForm setForm={setForm} />
+      )}
+    </div>
+  );
 };
 
 export default Login;
