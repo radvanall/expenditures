aa2aad896bd3e81366164008a3ce555db3bd3d94
Radvan Alexandru <rip777ton@gmail.com>
2023-05-14 22:35:44 +0300

    Add the Provider wrapper in the index.tsx that allow access to global context to all elements in the routes. Wrap the App component in the Routes component and asign to it “/*” path.

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "./context/Provider";
 
 import App from "./App";
 
@@ -10,7 +11,11 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
