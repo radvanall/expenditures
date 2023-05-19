07f0203c0b54f64f34066252f0a4d71574ac652c
Radvan Alexandru <rip777ton@gmail.com>
2023-05-19 22:10:29 +0300

    Add the table component in the InvoiceTable component

--- front/src/components/InvoiceTable/InvoiceTable.tsx
+++ front/src/components/InvoiceTable/InvoiceTable.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { FC } from "react";
 import styles from "./InvoiceTable.module.css";
-const InvoiceTable = () => {
-  return <div>InvoiceTable</div>;
+import Table from "../Table/Table";
+import { useInvoice } from "../../context/InvoiceContext/InvoiceContext";
+interface tableField {
+  // [index: string]: string | number;
+  id: number;
+  category: string;
+  item: string;
+  price: number;
+  quantity: number;
+  total: number;
+}
+interface Props {}
+const InvoiceTable: FC<Props> = () => {
+  const { tableData, handleEdit, handleDelete, handleSave } = useInvoice();
+  return (
+    <div>
+      <Table
+        tableFields={tableData}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+        handleSave={handleSave}
+      />
+    </div>
+  );
 };
 
 export default InvoiceTable;
