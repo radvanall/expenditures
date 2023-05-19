5e6ecf302b77d8f72b32e6e2ce92819159cccd8f
Radvan Alexandru <rip777ton@gmail.com>
2023-05-19 22:09:04 +0300

    Create the invoice form component

--- front/src/components/InvoiceForm/InvoiceForm.tsx
+++ front/src/components/InvoiceForm/InvoiceForm.tsx
@@ -1,187 +1,196 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./InvoiceForm.module.css";
-import useGet from "../../services/hooks/useGet";
-import api from "../../services/hooks/api";
-import Select from "../Select/Select";
-import { AiFillPoundCircle } from "react-icons/ai";
-import { newItem } from "../../data/loginInputFields";
+import { SelectI } from "../../Interfaces/SelectI";
+import { itemTypes, categoryTypes } from "../../Interfaces/keyConversionTypes";
+import {
+  MdOutlineProductionQuantityLimits,
+  MdPriceCheck,
+} from "react-icons/md";
+import { newItem, newCategory } from "../../data/loginInputFields";
 import { useMountTransition } from "../../services/hooks/useMountTransition";
+import AddCategoryModal from "../Modals/AddCategoryModal/AddCategoryModal";
 import AddItemModal from "../Modals/AddItemModal/AddItemModal";
-interface apiData {
-  category_id: string;
-  category_name: string;
-  items: { item_id: string; item_name: string; unit: string }[];
-}
-// interface Select {
-//   id: number;
-//   name: string;
-//   links?: string | number;
-// }
-interface SelectI {
-  id: number | null | string;
-  name: string;
-  links?: number | null;
-}
-
+import useGetRequest from "../../services/hooks/useGetRequest";
+import BasicInput from "../Inputs/BasicInput/BasicInput";
+import BasicButton from "../Buttons/BasicButton/BasicButton";
+import SelectFormField from "../SelectFormField/SelectFormField";
+import InputFormField from "../InputFormField/InputFormField";
+import { useInvoice } from "../../context/InvoiceContext/InvoiceContext";
 const InvoiceForm = () => {
-  // const { getData } = useGet(
-  //   "http://localhost:84/expenditures/public/category.php?request=categoriesAndItems"
-  // );
+  const {
+    getReg,
+    formState,
+    dateState,
+    handleDateChange,
+    submit,
+    handleChangeCategory,
+    defaultCategory,
+    getError,
+    handleChangeItem,
+    defaultItem,
+    submitChanges,
+    setFormState,
+  } = useInvoice();
   const [modal, setModal] = useState<boolean>(false);
-  const hasTransitionedIn = useMountTransition(modal, 1000);
-  const [categories, setCategories] = useState<SelectI[] | null>(null);
-  const [defaultCategory, setDefaultCategory] = useState<SelectI | null>(null);
-  const [items, setItems] = useState<SelectI[] | null>(null);
+  const [itemModal, setItemModal] = useState<boolean>(false);
+  const hasTransitionedIn = useMountTransition(modal, 300);
+  const hasItemTransitionedIn = useMountTransition(itemModal, 300);
   const [displayedItems, setDisplayedItems] = useState<SelectI[] | null>(null);
-  const [defaultItem, setDefaultItem] = useState<SelectI | null>(null);
   const [displayedCategories, setDisplayedCategories] = useState<
     SelectI[] | null
   >(null);
-  const ref = useRef<HTMLSelectElement>(null);
-  console.log(ref);
+  const {
+    data: items,
+    loading: itemsLoading,
+    error: itemsError,
+    fetchData: fetchItems,
+  } = useGetRequest<SelectI>("/item.php?id=all", itemTypes);
+  const {
+    data: categories,
+    loading: categoriesLoading,
+    error: categoriesError,
+    fetchData: fetchCategories,
+  } = useGetRequest<SelectI>("/category.php?id=all", categoryTypes);
   useEffect(() => {
-    const fetchCategories = async () => {
-      try {
-        const response = await api.get(
-          "/category.php?request=categoriesAndItems",
-          { withCredentials: true }
-        );
-        console.log(response.data);
-        const categoryArray = response.data.map((item: apiData) => ({
-          id: item.category_id,
-          name: item.category_name,
-        }));
-        setCategories([...categoryArray]);
-        setDisplayedCategories([...categoryArray]);
-        console.log("categories:", categories);
-        const itemArray = response.data.flatMap((category: apiData) => {
-          const items = category.items.map((item) => ({
-            id: item.item_id,
-            name: item.item_name,
-            unit: item.unit,
-            links: category.category_id,
-          }));
-          return items;
-          // id:
-          // name: item.category_name,
-          // links:
-        });
-        console.log(itemArray);
-        setItems(itemArray);
-        setDisplayedItems(itemArray);
-        console.log("items:", items);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchCategories();
-  }, []);
-  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    console.log(e.target.value);
-    const selectedOption = ref.current?.options[ref.current.selectedIndex];
-    const category = selectedOption?.dataset.category;
-    console.log(category);
-  };
-  const handleChangeCategory = (item: Select) => {
-    const newArray = items?.filter((value) => Number(value.links) === item.id);
-    setDisplayedItems(newArray ?? null);
-    setDefaultItem({
-      id: null,
-      name: "",
-      links: null,
-    });
-    console.log(newArray);
-  };
-  const handleChangeItem = (item: Select) => {
-    const newArray = categories?.filter(
-      (category) => Number(category.id) === Number(item.links)
-    );
-    if (newArray) {
-      setDefaultCategory({
-        id: newArray[0].id,
-        name: newArray[0].name,
-        links: newArray[0]?.links,
-      });
-    }
+    setDisplayedItems(items);
+  }, [items]);
+  useEffect(() => {
+    setDisplayedCategories(categories);
+  }, [categories]);
 
-    //setDisplayedCategories(newArray ?? null);
-    console.log("newArray:", newArray);
-  };
   const showAll = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setDisplayedItems(items);
   };
-  const createNewItem = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const createNewCategory = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setModal(true);
   };
+  const createNewItem = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setItemModal(true);
+  };
+  const regQuantity = getReg("quantity");
+  const regPrice = getReg("price");
+  const modifyCategory = (item: SelectI) => {
+    handleChangeCategory(
+      item,
+      items,
+      categories,
+      setDisplayedItems,
+      setDisplayedCategories
+    );
+  };
+  const modifyItem = (item: SelectI) => {
+    handleChangeItem(item, items, categories);
+  };
+  console.log("invoiceformUp");
   return (
-    <div>
+    <div className={styles.form__wrapper}>
+      <h4 className={styles.form__title}>
+        {formState === "create" ? "Enter the expenditure:" : "Edit the field"}
+      </h4>
       {(modal || hasTransitionedIn) && (
-        <AddItemModal
+        <AddCategoryModal
           visible={modal && hasTransitionedIn}
           setVisible={setModal}
-          // changedField={changedField}
-          // value={selectedUserField}
+          fetchData={fetchCategories}
+          inputFields={newCategory}
+        />
+      )}
+      {(itemModal || hasItemTransitionedIn) && (
+        <AddItemModal
+          visible={itemModal && hasItemTransitionedIn}
+          setVisible={setItemModal}
+          fetchData={fetchItems}
           inputFields={newItem}
         />
       )}
-      <form>
-        <Select
+      <div className={styles.input__wrapper}>
+        <BasicInput
+          type="date"
+          label="Date"
+          name="date"
+          value={
+            dateState?.toISOString().split("T")[0] ||
+            new Date().toISOString().slice(0, 10)
+          }
+          onChange={handleDateChange}
+        />
+      </div>
+      <form onSubmit={submit}>
+        <SelectFormField
+          name={"Select category"}
+          label={"Select category"}
           options={categories}
           displayedOptions={displayedCategories}
-          handleCallback={handleChangeCategory}
+          handleCallback={modifyCategory}
           defaultValue={defaultCategory}
           setDisplayedOptions={setDisplayedCategories}
           z_index={4}
-        />
-        <div>
-          <Select
-            options={items}
-            displayedOptions={displayedItems}
-            setDisplayedOptions={setDisplayedItems}
-            defaultValue={defaultItem}
-            handleCallback={handleChangeItem}
-          />
-          <button onClick={showAll}>All</button>
-          <button onClick={createNewItem}>New</button>
-        </div>
-
-        <label htmlFor="date">Date:</label>
-        <input
-          type="date"
-          name="date"
-          id=""
-          defaultValue={new Date().toISOString().slice(0, 10)}
-        />
-        <label htmlFor="category">Category:</label>
-        <select name="category" id="category" defaultValue="">
-          <option value={1}>---</option>
-        </select>
-        <label htmlFor="item">Item:</label>
-        <select
-          name="item"
-          id="item"
-          defaultValue=""
-          ref={ref}
-          onChange={handleSelectChange}
+          errors={getError("category_name")}
         >
-          <option value="" data-category="1">
-            ---
-          </option>
-          <option value={1} data-category="1">
-            Option 1
-          </option>
-          <option value="2" data-category="2">
-            Option 2
-          </option>
-          <option value="3" data-category="xcxcxc">
-            Option 3
-          </option>
-        </select>
+          <BasicButton text="New" handleClick={createNewCategory} />
+        </SelectFormField>
+        <SelectFormField
+          name={"Select item"}
+          label={"Select item"}
+          options={items}
+          displayedOptions={displayedItems}
+          handleCallback={modifyItem}
+          defaultValue={defaultItem}
+          setDisplayedOptions={setDisplayedItems}
+          errors={getError("item_name")}
+        >
+          <BasicButton text="All" handleClick={showAll} />
+          <BasicButton text="New" handleClick={createNewItem} />
+        </SelectFormField>
+
+        <InputFormField
+          type="number"
+          name={regQuantity?.name ?? ""}
+          onChange={regQuantity?.onChange}
+          onBlur={regQuantity?.onBlur}
+          inputRef={regQuantity?.ref}
+          label="Quantity"
+          errors={getError("quantity")}
+          afterText={defaultItem?.unit}
+        >
+          <MdOutlineProductionQuantityLimits />
+        </InputFormField>
+
+        <InputFormField
+          type="number"
+          name={regPrice?.name ?? ""}
+          onChange={regPrice?.onChange}
+          onBlur={regPrice?.onBlur}
+          inputRef={regPrice?.ref}
+          width="75%"
+          label="Price per unit"
+          errors={getError("price")}
+          afterText={"lei"}
+        >
+          <MdPriceCheck />
+        </InputFormField>
+
+        {formState === "create" ? (
+          <BasicButton text="Add record" type="submit" />
+        ) : (
+          <div>
+            <BasicButton
+              text="Edit"
+              type="submit"
+              handleClick={submitChanges}
+            />{" "}
+            <BasicButton
+              text="Cancel"
+              type="button"
+              handleClick={() => setFormState("create")}
+            />
+          </div>
+        )}
       </form>
     </div>
   );
 };
-
 export default InvoiceForm;
