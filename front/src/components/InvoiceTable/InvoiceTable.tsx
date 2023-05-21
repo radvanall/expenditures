2754910e2478084955aaf9efe700ddad7787a245
Radvan Alexandru <rip777ton@gmail.com>
2023-05-21 22:49:28 +0300

    Add the InvoiceProvider to newInvoice page. Add modals that are used to display messages from server to InvoiceTable component.Move the footer from Table element to InvoiceTable element.Add the  save and delete all records buttons to InvoiceTable.Add styles for newInvoice and InvoiceTable components

--- front/src/components/InvoiceTable/InvoiceTable.tsx
+++ front/src/components/InvoiceTable/InvoiceTable.tsx
@@ -1,9 +1,12 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import styles from "./InvoiceTable.module.css";
 import Table from "../Table/Table";
 import { useInvoice } from "../../context/InvoiceContext/InvoiceContext";
+import Pending from "../Pending/Pending";
+import BasicButton from "../Buttons/BasicButton/BasicButton";
+import { useMountTransition } from "../../services/hooks/useMountTransition";
+import MessageModal from "../Modals/MessageModal/MessageModal";
 interface tableField {
-  // [index: string]: string | number;
   id: number;
   category: string;
   item: string;
@@ -11,17 +14,76 @@ interface tableField {
   quantity: number;
   total: number;
 }
-interface Props {}
-const InvoiceTable: FC<Props> = () => {
-  const { tableData, handleEdit, handleDelete, handleSave } = useInvoice();
+const InvoiceTable = () => {
+  const {
+    tableData,
+    handleEdit,
+    handleDelete,
+    handleSave,
+    handleResetRecords,
+    pending,
+    error,
+    answer,
+  } = useInvoice();
+  const [visible, setVisible] = useState(false);
+  const [serverAnswer, setServerAnswer] = useState(false);
+  const tableTitle = "Records";
+  const submitRecords = () => {
+    setServerAnswer(true);
+    handleSave();
+  };
+  const [totalPrice, setTotalPrice] = useState(0);
+  useEffect(() => {
+    setTotalPrice(
+      tableData
+        ? tableData?.reduce((accumulator, field) => {
+            return accumulator + field.total;
+          }, 0)
+        : 0
+    );
+  }, [tableData]);
   return (
-    <div>
-      <Table
+    <div className={styles.table__wrapper}>
+      <MessageModal
+        visible={visible}
+        setVisible={setVisible}
+        handleOk={handleResetRecords}
+        cancelButton={true}
+      >
+        <h4>{"The records will be lost."}</h4>
+      </MessageModal>
+
+      <MessageModal
+        visible={serverAnswer}
+        setVisible={setServerAnswer}
+        handleOk={() => setServerAnswer(false)}
+      >
+        {pending ? <Pending /> : error ? <h4>{error}</h4> : <h4>{answer}</h4>}
+      </MessageModal>
+
+      <Table<tableField>
+        tableTitle={tableTitle}
         tableFields={tableData}
         handleEdit={handleEdit}
         handleDelete={handleDelete}
-        handleSave={handleSave}
       />
+      <div className={styles.footer}>
+        <h4>{`Total price: ${totalPrice}`}</h4>
+      </div>
+      <div className={styles.buttons__wrapper}>
+        <BasicButton
+          text="Save"
+          height="26px"
+          color="blue"
+          handleClick={submitRecords}
+        />
+        <BasicButton
+          text="Delete all records"
+          height="26px"
+          color="pink"
+          handleClick={() => setVisible(true)}
+        />
+      </div>
     </div>
   );
 };
--- front/src/pages/NewInvoice/NewInvoice.tsx
+++ front/src/pages/NewInvoice/NewInvoice.tsx
@@ -1,16 +1,18 @@
-import React from "react";
 import styles from "./NewInvoice.module.css";
 import InvoiceForm from "../../components/InvoiceForm/InvoiceForm";
 import InvoiceTable from "../../components/InvoiceTable/InvoiceTable";
+import { InvoiceProvider } from "../../context/InvoiceContext/InvoiceContext";
 const NewInvoice = () => {
   return (
     <div className={styles.invoice_wrapper}>
-      <div className={styles.invoice_form}>
-        <InvoiceForm />
-      </div>
-      <div className={styles.invoice_table}>
-        <InvoiceTable />
-      </div>
+      <InvoiceProvider>
+        <div className={styles.invoice_form}>
+          <InvoiceForm />
+        </div>
+        <div className={styles.invoice_table}>
+          <InvoiceTable />
+        </div>
+      </InvoiceProvider>
     </div>
   );
 };
