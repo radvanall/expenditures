1b7443ec81dd997c4e503ce946d12a8dbbb01e1c
Radvan Alexandru <rip777ton@gmail.com>
2023-05-21 22:52:14 +0300

    Create a hook that is used to detect when a key is pressed

---
+++ front/src/services/hooks/useKeyPress.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import React from "react";
+
+export const useKeyPress = (targetKey: string) => {
+  const [keyPressed, setKeyPressed] = useState(false);
+  useEffect(() => {
+    const downHandler = ({ key }: { key: string }) => {
+      if (key === targetKey) {
+        setKeyPressed(true);
+      }
+    };
+    const upHandler = ({ key }: { key: string }) => {
+      if (key === targetKey) {
+        setKeyPressed(false);
+      }
+    };
+    window.addEventListener("keydown", downHandler);
+    window.addEventListener("keyup", upHandler);
+    return () => {
+      window.removeEventListener("keydown", downHandler);
+      window.removeEventListener("keyup", upHandler);
+    };
+  }, [targetKey]);
+  return keyPressed;
+};
