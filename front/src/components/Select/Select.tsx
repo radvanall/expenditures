08de8c578e511b1955c44bf2cf853a64544f21ea
Radvan Alexandru <rip777ton@gmail.com>
2023-05-24 20:41:00 +0300

    Change the simple input to custom react element BasicInput. Add styles for ul list of custom select element. Add scrollbar for ul

--- front/src/components/Select/Select.tsx
+++ front/src/components/Select/Select.tsx
@@ -1,139 +1,95 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useState, useReducer, useRef } from "react";
 import styles from "./Select.module.css";
 import { AiFillCaretDown } from "react-icons/ai";
-interface Select {
-  id: number | null | string;
-  name: string;
-  links?: number | null;
-}
+import { SelectI } from "../../Interfaces/SelectI";
+import BasicInput from "../Inputs/BasicInput/BasicInput";
+import useSelect from "./useSelect";
+
 interface Props {
-  //   options: { id: number; name: string; links?: string | number }[] | null;
   z_index?: number;
-  defaultValue?: Select | null;
-  options: Select[] | null;
-  displayedOptions?: Select[] | null;
-  setDisplayedOptions: React.Dispatch<React.SetStateAction<Select[] | null>>;
-  handleCallback?: (id: Select) => void;
+  name: string;
+  label: string;
+  defaultValue?: SelectI | null;
+  options: SelectI[] | null;
+  displayedOptions?: SelectI[] | null;
+  setDisplayedOptions: React.Dispatch<React.SetStateAction<SelectI[] | null>>;
+  handleCallback?: (id: SelectI) => void;
+  color?: string;
+  width?: string;
 }
 const Select: FC<Props> = ({
   z_index,
   defaultValue,
+  name,
+  label,
+  width,
   options,
   displayedOptions,
   setDisplayedOptions,
   handleCallback,
+  color,
 }) => {
-  //   const [displayedOptions, setDisplayedOptions] = useState(options);
-  //   useEffect(() => {
-  //     console.log("Selects select:", options);
-  //     setDisplayedOptions(options);
-  //   }, [options]);
-  useEffect(() => {
-    if (defaultValue) setValue(defaultValue);
-  }, [defaultValue]);
-  const [visible, setVisible] = useState(false);
-  const [value, setValue] = useState<Select>({
-    id: null,
-    name: "",
-    links: null,
-  });
-  const toggleOptions = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    setVisible((prev) => !prev);
-  };
-  const handleSelect = (e: React.MouseEvent<HTMLUListElement>) => {
-    setVisible(false);
-    if (e.target instanceof HTMLLIElement) {
-      //   console.log(e.target);
-      // e.target.innerText
-      console.log(e.target.getAttribute("links"));
-      console.log(e.target.getAttribute("data-id"));
-      //   setValue(e.target.innerText);
-      const object = {
-        id: Number(e.target.getAttribute("data-id")),
-        name: (e.target as HTMLLIElement).innerText,
-        links: Number(e.target.getAttribute("data-links")),
-      };
-      setValue(object);
-      if (handleCallback) {
-        handleCallback(object);
-      }
-    }
-  };
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(value);
-    setVisible(true);
-    setValue({
-      id: "new",
-      name: e.target.value,
-      links: null,
-    });
-    const newArray = options?.filter((item) =>
-      item.name.toLowerCase().includes(e.target.value.toLowerCase())
-    );
-    console.log(newArray);
-    setDisplayedOptions(newArray ?? null);
-    newArray?.length ? setVisible(true) : setVisible(false);
-  };
-  const handleBlur = () => {
-    console.log("name:", value.name);
-    console.log(
-      "some:",
-      options?.some(
-        (option) => option.name.toLowerCase() === value.name.toLowerCase()
-      )
-    );
-    if (
-      !options?.some(
-        (option) => option.name.toLowerCase() === value.name.toLowerCase()
-      )
-    ) {
-      setValue({
-        id: null,
-        name: "",
-        links: null,
-      });
-      //   setDisplayedOptions(options);
-      //   setVisible(false);
-    }
-    //setVisible(true);
-    setTimeout(() => {
-      setDisplayedOptions(options);
-    }, 100);
-  };
+  const {
+    toggleOptions,
+    handleBlur,
+    handleInputChange,
+    handleSelect,
+    liRefs,
+    visible,
+    state,
+  } = useSelect(options, setDisplayedOptions, defaultValue, handleCallback);
+
   return (
-    <div className={styles.select__wrapper}>
-      <label htmlFor="select">name</label>
+    <div
+      className={
+        color === "pink"
+          ? `${styles.select__wrapper} ${styles.pink}`
+          : `${styles.select__wrapper} ${styles.blue}`
+      }
+      style={{ width: width ?? "100%" }}
+      data-list="drop_down_list"
+    >
       <div className={styles.input__wrapper}>
-        <input
-          onBlur={handleBlur}
+        <BasicInput
           type="text"
-          name="select"
-          value={value.name}
+          onBlur={handleBlur}
           onChange={handleInputChange}
+          name={name}
+          label={label}
+          value={state.value.name}
         />
-        <button onClick={toggleOptions}>
+        <button onClick={toggleOptions} type="button">
           <AiFillCaretDown
             className={visible ? `${styles.icon} ${styles.up}` : styles.icon}
           />
         </button>
       </div>
-      <ul
+      <div
+        className={
+          visible ? `${styles.menu_list} ${styles.visible}` : styles.menu_list
+        }
         style={{ zIndex: z_index ?? 3 }}
-        className={visible ? styles.ul_visible : undefined}
-        onMouseDown={handleSelect}
       >
-        {displayedOptions &&
-          displayedOptions.map((option) => (
-            <li
-              key={option.id}
-              data-links={option?.links ?? ""}
-              data-id={option?.id}
-            >
-              {option.name}
-            </li>
-          ))}
-      </ul>
+        <ul className={styles.list} onMouseDown={handleSelect}>
+          {displayedOptions &&
+            displayedOptions.map((option, index) => (
+              <li
+                className={
+                  Number(index) === state.selected && visible
+                    ? styles.selected
+                    : ""
+                }
+                ref={(ref) => (liRefs.current[index] = ref)}
+                key={option.id}
+                data-links={option?.links ?? ""}
+                data-id={option?.id}
+                data-index={index}
+              >
+                {option.name}
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 };
