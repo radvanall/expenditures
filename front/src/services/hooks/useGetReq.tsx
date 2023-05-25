8947df9d6c7fbc0025c676b8debd7907b2ed9b57
Radvan Alexandru <rip777ton@gmail.com>
2023-05-25 21:25:46 +0300

    Create the useGetReq hook that is a generic function that recives data from server

---
+++ front/src/services/hooks/useGetReq.tsx
@@ -0,0 +1,36 @@
+import axios, { AxiosError } from "axios";
+import React, { useEffect, useState } from "react";
+import api from "./api";
+
+export const useGetReq = <T,>(url: string) => {
+  const [data, setData] = useState<T>();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await api.get(url);
+      setData(response.data);
+      setError(null);
+    } catch (err) {
+      if (!axios.isAxiosError(err)) {
+        setError("Server does not answer");
+        return;
+      }
+      const axiosError = err as AxiosError<{ error: string }>;
+      if (axiosError.response) {
+        console.log("axiosError:", axiosError);
+        setError(axiosError.response.data?.error);
+      } else {
+        setError("Network Error");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return { data, loading, error, fetchData };
+};
+export default useGetReq;
