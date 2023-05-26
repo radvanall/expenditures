1a4e03d314f890713053070cba7cfc457d00707a
Radvan Alexandru <rip777ton@gmail.com>
2023-05-26 20:14:32 +0300

    Add the checkbox and NasicInput elements in the FilterMenu component.Add styles for input__wrapper

--- front/src/components/FilterMenu/FilterMenu.tsx
+++ front/src/components/FilterMenu/FilterMenu.tsx
@@ -1,40 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import BasicInput from "../Inputs/BasicInput/BasicInput";
 import styles from "./FilterMenu.module.css";
-import RadioButton from "../RadioButton/RadioButton";
+import Radiobox from "../Radiobox/Radiobox";
 import Checkbox from "../Checkbox/Checkbox";
+import BasicButton from "../Buttons/BasicButton/BasicButton";
 const FilterMenu = () => {
+  const defaultFormValues = {
+    date_radio: "",
+    date: new Date().toISOString().slice(0, 10),
+    first_date: new Date().toISOString().slice(0, 10),
+    last_date: new Date().toISOString().slice(0, 10),
+  };
+  const [formData, setFormData] = useState(defaultFormValues);
+  const [ckecked, setChecked] = useState(false);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+  const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
   return (
     <div className={styles.menu__wrapper}>
-      <Checkbox />
-      {/* <RadioButton /> */}
-      {/* <div className={styles.radio__button}>
-        <label className={styles.radio}>
-          <input
-            type="radio"
-            value="first"
-            name="date"
-            onChange={(e) => console.log(e.target.value)}
+      <button onClick={() => console.log(ckecked)}>Click</button>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.checkbox__wrapper}>
+          {/* <Checkbox
+          name="check"
+          id="dateid"
+          label="Select date"
+          onChange={(e) => setChecked(e.currentTarget.checked)}
+        /> */}
+        </div>
+        <div className={styles.checkbox__wrapper}>
+          <Radiobox
+            id="date"
+            name="date_radio"
+            label="chousedate"
+            value="date"
+            onChange={handleFormChange}
           />
-          Select date
-          <span></span>
-        </label>
-      </div> */}
-      {/* <div className={styles.radio__button}>
-        <label className={styles.radio}>
-          <input type="radio" value="second" name="date" />
-          Select a range of dates
-          <span></span>
-        </label>
-      </div> */}
-
-      <BasicInput
-        type="date"
-        label=""
-        name="date"
-        value={new Date().toISOString().slice(0, 10)}
-        //   onChange={handleDateChange}
-      />
+        </div>
+        <BasicInput
+          type="date"
+          label=""
+          name="date"
+          value={formData.date}
+          onChange={handleFormChange}
+        />
+        <div className={styles.checkbox__wrapper}>
+          <Radiobox
+            id="date2"
+            name="date_radio"
+            label="chouseRangeofdate"
+            value="range"
+            onChange={handleFormChange}
+          />
+        </div>
+        <div className={styles.input__wrapper}>
+          <BasicInput
+            type="date"
+            label="First date"
+            name="first_date"
+            value={formData.first_date}
+            onChange={handleFormChange}
+          />
+        </div>
+        <div className={styles.input__wrapper}>
+          <BasicInput
+            type="date"
+            label="Last date"
+            name="last_date"
+            value={formData.last_date}
+            onChange={handleFormChange}
+          />
+        </div>
+        <BasicButton type="submit" text="Filter" />
+      </form>
     </div>
   );
 };
