ad174a01577a577efa3de92d7b2406ad39ab1d84
Radvan Alexandru <rip777ton@gmail.com>
2023-05-26 20:12:04 +0300

    Create a hook that is used for pagination.

---
+++ front/src/services/hooks/usePagination.ts
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+export const usePagination = () => {
+  const [rowCount, setRowCount] = useState(0);
+  const [offset, setOffset] = useState(10);
+  const [firstRow, setFistRow] = useState(0);
+  const [nrOfPages, setNrOfPages] = useState(0);
+  const [selected, setSelected] = useState(1);
+  const changeFirstRow = (nr: number) => {
+    setFistRow(nr * offset - offset);
+    setSelected(nr);
+    console.log("first row:", firstRow);
+    console.log("rowCount:", rowCount);
+  };
+  const goToStart = () => {
+    setSelected(1);
+    setFistRow(0);
+  };
+  const goToEnd = () => {
+    if (rowCount) {
+      setFistRow(Math.ceil(rowCount / offset) * offset - offset);
+      setSelected(nrOfPages);
+    }
+  };
+  useEffect(() => {
+    if (rowCount) setNrOfPages(Math.ceil(rowCount / offset));
+  }, [rowCount]);
+  return {
+    offset,
+    firstRow,
+    nrOfPages,
+    selected,
+    changeFirstRow,
+    goToStart,
+    goToEnd,
+    setRowCount,
+    setOffset,
+  };
+};
