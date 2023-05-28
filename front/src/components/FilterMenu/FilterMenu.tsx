334c6e233c2ea5978b14126a66a92833a92851f0
Radvan Alexandru <rip777ton@gmail.com>
2023-05-28 21:44:28 +0300

    Add the range input for min and max price.Use the use context hook to get the data instead of using props

--- front/src/components/FilterMenu/FilterMenu.tsx
+++ front/src/components/FilterMenu/FilterMenu.tsx
@@ -4,29 +4,46 @@ import styles from "./FilterMenu.module.css";
 import Radiobox from "../Radiobox/Radiobox";
 import Checkbox from "../Checkbox/Checkbox";
 import BasicButton from "../Buttons/BasicButton/BasicButton";
+import RangeInput from "../RangeInput/RangeInput";
+import { useInvoicesContext } from "../../pages/Invoices/InvoincesContext";
 const FilterMenu = () => {
-  const defaultFormValues = {
-    date_radio: "",
-    date: new Date().toISOString().slice(0, 10),
-    first_date: new Date().toISOString().slice(0, 10),
-    last_date: new Date().toISOString().slice(0, 10),
-  };
-  const [formData, setFormData] = useState(defaultFormValues);
-  const [ckecked, setChecked] = useState(false);
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(formData);
-  };
-  const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prevValues) => ({
-      ...prevValues,
-      [name]: value,
-    }));
-  };
+  // const defaultFormValues = {
+  //   date_radio: "",
+  //   date: new Date().toISOString().slice(0, 10),
+  //   first_date: new Date().toISOString().slice(0, 10),
+  //   last_date: new Date().toISOString().slice(0, 10),
+  //   min_price_checkbox: false,
+  //   min_price: 0,
+  //   max_price_checkbox: false,
+  //   max_price: 0,
+  // };
+  // const [rangeValue, setRengeValue] = useState(0);
+  // const [formData, setFormData] = useState(defaultFormValues);
+  // const [ckecked, setChecked] = useState(false);
+  // const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   console.log(formData);
+  // };
+  // const handleFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const { name, value } = e.target;
+  //   if (name === "min_price_checkbox" || name === "max_price_checkbox") {
+  //     setFormData((prevValues) => ({
+  //       ...prevValues,
+  //       [name]: e.target.checked,
+  //     }));
+  //     return;
+  //   }
+  //   setFormData((prevValues) => ({
+  //     ...prevValues,
+  //     [name]: value,
+  //   }));
+  // };
+  const { handleSubmit, handleFormChange, formData, data } =
+    useInvoicesContext();
+
   return (
     <div className={styles.menu__wrapper}>
-      <button onClick={() => console.log(ckecked)}>Click</button>
+      {/* <button onClick={() => console.log(ckecked)}>Click</button> */}
       <form onSubmit={handleSubmit}>
         <div className={styles.checkbox__wrapper}>
           {/* <Checkbox
@@ -79,6 +96,36 @@ const FilterMenu = () => {
             onChange={handleFormChange}
           />
         </div>
+        <div className={styles.checkbox__wrapper}>
+          <Checkbox
+            name="min_price_checkbox"
+            label="Minimum price"
+            id="min_price"
+            onChange={handleFormChange}
+          />
+        </div>
+        <RangeInput
+          name="min_price"
+          value={formData.min_price}
+          max={data?.max_price}
+          unit="Lei"
+          onChange={handleFormChange}
+        />
+        <div className={styles.checkbox__wrapper}>
+          <Checkbox
+            name="max_price_checkbox"
+            label="Maximum price"
+            id="max_price"
+            onChange={handleFormChange}
+          />
+        </div>
+        <RangeInput
+          name="max_price"
+          value={formData.max_price}
+          max={data?.max_price}
+          unit="Lei"
+          onChange={handleFormChange}
+        />
         <BasicButton type="submit" text="Filter" />
       </form>
     </div>
