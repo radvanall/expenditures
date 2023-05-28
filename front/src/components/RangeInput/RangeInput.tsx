cf32deda9277424a03968d3e582bdc0d3eda3e93
Radvan Alexandru <rip777ton@gmail.com>
2023-05-28 21:41:12 +0300

    Add the rangeInput folder

---
+++ front/src/components/RangeInput/RangeInput.tsx
@@ -0,0 +1,36 @@
+import React, { FC, HTMLAttributes, InputHTMLAttributes } from "react";
+import styles from "./RangeInput.module.css";
+interface RangeInput extends HTMLAttributes<HTMLInputElement> {
+  name: string;
+  value?: number;
+  unit?: string;
+  min?: number;
+  max?: number;
+}
+const RangeInput: FC<RangeInput> = ({
+  name,
+  min,
+  max,
+  value,
+  unit = "",
+  ...props
+}) => {
+  return (
+    <div className={styles.range__input__container}>
+      <div className={styles.range__input__wrapper}>
+        <input
+          type="range"
+          name={name}
+          value={value}
+          min={min ?? 0}
+          max={max ?? 10000}
+          {...props}
+        />
+        <progress value={value} max={max ?? 10000}></progress>
+      </div>
+      <label>{value + " " + unit}</label>
+    </div>
+  );
+};
+
+export default RangeInput;
