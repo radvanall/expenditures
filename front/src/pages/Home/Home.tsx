81319998ad7a9ae14169dfdcc4b5f9f8de809fd5
Radvan Alexandru <rip777ton@gmail.com>
2023-06-01 20:55:29 +0300

    Add the grid layout on the Home.tsx page. Add the MoneyPerDayChart on the Home.tsx page.

--- front/src/pages/Home/Home.tsx
+++ front/src/pages/Home/Home.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import UserCard from "../../components/Cards/UserCard/UserCard";
-
+import styles from "./Home.module.css";
+import MoneyPerDayChart from "../../components/Charts/MoneyPerDayChart/MoneyPerDayChart";
 const Home = () => {
   return (
-    <div>
+    <div className={styles.home__wrapper}>
       <UserCard />
+      <div className={styles.grid_col_span_3}>
+        <MoneyPerDayChart />
+      </div>
+      <div className={styles.grid_col_span_2}> df</div>
+      <div className={styles.grid_col_span_2}>dfs</div>
     </div>
   );
 };
