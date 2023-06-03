4ce490bd1478ac0c28df9f430b2ec46a0fee0a25
Radvan Alexandru <rip777ton@gmail.com>
2023-06-03 18:59:21 +0300

    Add the CateoryCard in Home component

--- front/src/pages/Home/Home.tsx
+++ front/src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import UserCard from "../../components/Cards/UserCard/UserCard";
 import styles from "./Home.module.css";
 import MoneyPerDayChart from "../../components/Charts/MoneyPerDayChart/MoneyPerDayChart";
+import CategoryCard from "../../components/Cards/Card/CategoryCard/CategoryCard";
 const Home = () => {
   return (
     <div className={styles.home__wrapper}>
@@ -9,8 +10,11 @@ const Home = () => {
       <div className={styles.grid_col_span_3}>
         <MoneyPerDayChart />
       </div>
-      <div className={styles.grid_col_span_2}> df</div>
-      <div className={styles.grid_col_span_2}>dfs</div>
+      <div className={styles.grid_col_span_2}>
+        <CategoryCard />
+        {/* <ProgressBar max={1000000000} value={100000000} /> */}
+      </div>
+      <div className={styles.grid_col_span_2}></div>
     </div>
   );
 };
