45e38f99be4e183af531cf43cacde719987d860c
Radvan Alexandru <rip777ton@gmail.com>
2023-06-04 23:10:29 +0300

    Create get  request to server that recives the data for category table.Create for each total price field a progressbar and saved the new data in the categories array. Add the category table and send the data to it.

--- front/src/components/Cards/Card/CategoryCard/CategoryCard.tsx
+++ front/src/components/Cards/Card/CategoryCard/CategoryCard.tsx
@@ -1,23 +1,110 @@
 import React, { useEffect, useState } from "react";
+import useGetReq from "../../../../services/hooks/useGetReq";
 import ProgressBar, { ProgressBarI } from "../../../ProgressBar/ProgressBar";
+import Table from "../../../Table/Table";
 import Card from "../Card";
+type categoriesT = {
+  id: number;
+  "Category name": string;
+  "Number of items": number;
+  "Total price": number;
+};
+type CategoryTableT = {
+  categories: categoriesT[];
+};
 
+type categoriesElementsT = {
+  id: number;
+  "Category name": string;
+  "Number of items": number;
+  "Total price": JSX.Element;
+};
 const CategoryCard = () => {
-  const [progress, setProgress] = useState<ProgressBarI[]>([]);
-  useEffect(() => {
-    const newDivs: ProgressBarI[] = [];
-    for (let i = 98; i <= 105; i++) {
-      newDivs.push({ value: i, max: 105 });
-    }
-    setProgress(newDivs);
-  }, []);
+  const { data, loading, error, fetchData } = useGetReq<CategoryTableT>(
+    "http://localhost:84/expenditures/public/category.php?request=get_category_table"
+  );
+  const [cateogries, setCategories] = useState<categoriesElementsT[]>([]);
 
-  const createProgressBars = () => {
-    return progress.map((item) => (
-      <ProgressBar value={item.value} max={item.max} />
-    ));
-  };
-  return <Card>{createProgressBars()}</Card>;
+  const ion = <ProgressBar value={1} max={2} key={56} />;
+  useEffect(() => {
+    if (data) {
+      const totalPrice = data?.categories[0]["Total price"];
+      const newArray: categoriesElementsT[] = data?.categories.map(
+        (category, index) => {
+          return {
+            ...category,
+            ["Total price"]: (
+              <ProgressBar
+                value={category["Total price"] as number}
+                max={totalPrice as number}
+                additionalLabel="lei"
+                key={index}
+              />
+            ),
+          };
+        }
+      );
+      newArray.push({
+        id: 500,
+        "Category name": "test",
+        "Number of items": 34,
+        "Total price": (
+          <ProgressBar
+            value={40000000}
+            max={50000000}
+            additionalLabel="lei"
+            key={45}
+          />
+        ),
+      });
+      setCategories(newArray);
+    }
+
+    console.log(data);
+  }, [data]);
+  //   const [progress, setProgress] = useState<ProgressBarI[]>([]);
+  //   useEffect(() => {
+  //     const newDivs: ProgressBarI[] = [];
+  //     for (let i = 98; i <= 105; i++) {
+  //       newDivs.push({ value: i, max: 105 });
+  //     }
+  //     setProgress(newDivs);
+  //   }, []);
+
+  //   const createProgressBars = () => {
+  //     return progress.map((item) => (
+  //       <ProgressBar
+  //         value={item.value}
+  //         max={item.max}
+  //         key={item.value as number}
+  //       />
+  //     ));
+  //   };
+  return (
+    <Card>
+      {/* {createProgressBars()} */}
+      {/* {ion} */}
+      {
+        cateogries && (
+          <Table<categoriesElementsT>
+            tableTitle="Categories"
+            tableFields={cateogries}
+            buttonPading="10px"
+            handleEdit={() => {}}
+            handleDelete={() => {}}
+            customColumnWidth={{ columnName: "Total price", width: 40 }}
+          />
+        )
+        // cateogries.map((category) => category["Total price"])
+        // <ProgressBar
+        //   value={40000000}
+        //   max={50000000}
+        //   additionalLabel="lei"
+        //   // key={index}
+        // />
+      }
+    </Card>
+  );
 };
 
 export default CategoryCard;
