2b789c32a51bc8d2b53576e19918be16dcfdb780
Radvan Alexandru <rip777ton@gmail.com>
2023-06-05 21:44:12 +0300

    Add the additionalLabel props to ProgressBar.tsx that allows to send additional text.

--- front/src/components/ProgressBar/ProgressBar.tsx
+++ front/src/components/ProgressBar/ProgressBar.tsx
@@ -1,13 +1,15 @@
 import React, { FC } from "react";
 import styles from "./ProgressBar.module.css";
 export interface ProgressBarI
-  extends React.ProgressHTMLAttributes<HTMLProgressElement> {}
+  extends React.ProgressHTMLAttributes<HTMLProgressElement> {
+  additionalLabel?: string;
+}
 
-const ProgressBar: FC<ProgressBarI> = ({ max, value }) => {
+const ProgressBar: FC<ProgressBarI> = ({ max, value, additionalLabel }) => {
   return (
     <div className={styles.progress__bar__wrapper}>
       <progress max={max} value={value} className={styles.progress__bar} />
-      <label>{value}</label>
+      <label>{additionalLabel ? `${value}  ${additionalLabel}` : value}</label>
     </div>
   );
 };
