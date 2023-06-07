8018fc7322927594c6ba2b88695788144be874b5
Radvan Alexandru <rip777ton@gmail.com>
2023-06-07 20:38:59 +0300

    Add an loading state and used it to return a div if there is no data in tableFields props and used it instead of {tableFields && } check. This way the table does not flicker at first render

--- front/src/components/Table/Table.tsx
+++ front/src/components/Table/Table.tsx
@@ -2,22 +2,6 @@ import { PropsWithChildren, ReactNode, useEffect, useState } from "react";
 import styles from "./Table.module.css";
 import BasicButton from "../Buttons/BasicButton/BasicButton";
 
-// console.log(Object.keys(data[0]).length);
-// console.log(95 / 5);
-// interface tableField {
-//   id: number;
-//   category: string;
-//   item: string;
-//   price: number;
-//   quantity: number;
-//   total: number;
-// }
-// interface Props<T> {
-//   tableFields: tableField[] | undefined;
-//   handleEdit: (id: number) => void;
-//   handleDelete: (id: number) => void;
-//   tableTitle: string;
-// }
 interface Props<T> {
   tableFields: T[] | undefined;
   tableTitle?: string;
@@ -27,11 +11,6 @@ interface Props<T> {
   handleDelete?: (id: number) => void;
   handleDetails?: (id: number) => void;
 }
-// const Table: FC<Props<T>> = ({
-//   tableFields,
-//   handleEdit,
-//   handleDelete,
-//   tableTitle,
 function Table<T extends { id: number }>({
   tableFields,
   tableTitle,
@@ -43,92 +22,96 @@ function Table<T extends { id: number }>({
 }: PropsWithChildren<Props<T>>) {
   const tableHeader = tableFields?.length ? Object.keys(tableFields[0]) : null;
   const [columnWidth, setColumnWidth] = useState(0);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const restWidth = customColumnWidth ? 95 - customColumnWidth.width : 95;
     const lenghtMin = customColumnWidth ? 1 : 0;
     if (tableHeader !== null)
       setColumnWidth(restWidth / (tableHeader?.length - lenghtMin));
+    tableFields?.length ? setLoading(false) : setLoading(true);
   }, [tableFields]);
 
+  if (loading) {
+    return <div style={{ textAlign: "center" }}>No data.</div>;
+  }
   return (
     <table className={styles.table}>
       {tableTitle && <caption>{tableTitle}</caption>}
       <thead>
-        {tableFields?.length && (
-          <tr>
-            {tableHeader &&
-              tableHeader.map((columnName) => (
-                <th
-                  key={columnName}
-                  style={
-                    columnName === "id"
-                      ? { width: "5%" }
-                      : columnName === customColumnWidth?.columnName
-                      ? {
-                          width: `${customColumnWidth.width}%`,
-                          textAlign: "center",
-                        }
-                      : { width: `${columnWidth}%` }
-                  }
-                >
-                  {columnName}
-                </th>
-              ))}
-            <th style={{ width: `${columnWidth}%` }}>Actions</th>
-          </tr>
-        )}
+        <tr>
+          {tableHeader &&
+            tableHeader.map((columnName) => (
+              <th
+                key={columnName}
+                style={
+                  columnName === "id"
+                    ? { width: "5%" }
+                    : columnName === customColumnWidth?.columnName
+                    ? {
+                        width: `${customColumnWidth.width}%`,
+                        textAlign: "center",
+                      }
+                    : { width: `${columnWidth}%` }
+                }
+              >
+                {columnName}
+              </th>
+            ))}
+          <th style={{ width: `${columnWidth}%` }}>Actions</th>
+        </tr>
       </thead>
       <tbody>
-        {tableFields?.length &&
-          tableFields.map((row) => (
-            <tr key={row.id}>
-              {Object.keys(row).map((cell) => (
-                <td
-                  key={cell}
-                  style={
-                    cell === "id"
-                      ? { width: "5%" }
-                      : cell === customColumnWidth?.columnName
-                      ? { width: `${customColumnWidth.width}%` }
-                      : { width: `${columnWidth}%` }
-                  }
-                >
-                  {row[cell as keyof typeof row] as unknown as ReactNode}
-                </td>
-              ))}
+        {tableFields?.map((row, index = 1) => (
+          <tr key={row.id}>
+            {Object.keys(row).map((cell) => (
               <td
-                className={styles.actions__cell}
-                style={{
-                  width: `${columnWidth}%`,
-                  paddingLeft: buttonPading ?? "4px",
-                }}
+                key={cell}
+                style={
+                  cell === "id"
+                    ? { width: "5%" }
+                    : cell === customColumnWidth?.columnName
+                    ? { width: `${customColumnWidth.width}%` }
+                    : { width: `${columnWidth}%` }
+                }
               >
-                {handleDetails && (
-                  <BasicButton
-                    text="Details"
-                    handleClick={() => handleDetails(row["id"])}
-                    height="26px"
-                  />
-                )}
-
-                {handleEdit && (
-                  <BasicButton
-                    text="Edit"
-                    handleClick={() => handleEdit(row["id"])}
-                    height="26px"
-                  />
-                )}
-                {handleDelete && (
-                  <BasicButton
-                    text="Delete"
-                    height="26px"
-                    color="pink"
-                    handleClick={() => handleDelete(row["id"])}
-                  />
-                )}
+                {cell === "id"
+                  ? index + 1
+                  : (row[cell as keyof typeof row] as unknown as ReactNode)}
               </td>
-            </tr>
-          ))}
+            ))}
+            <td
+              className={styles.actions__cell}
+              style={{
+                width: `${columnWidth}%`,
+                paddingLeft: buttonPading ?? "4px",
+              }}
+            >
+              {handleDetails && (
+                <BasicButton
+                  text="Details"
+                  handleClick={() => handleDetails(row["id"])}
+                  height="26px"
+                />
+              )}
+
+              {handleEdit && (
+                <BasicButton
+                  text="Edit"
+                  handleClick={() => handleEdit(row["id"])}
+                  height="26px"
+                />
+              )}
+              {handleDelete && (
+                <BasicButton
+                  text="Delete"
+                  height="26px"
+                  color="pink"
+                  handleClick={() => handleDelete(row["id"])}
+                />
+              )}
+            </td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
