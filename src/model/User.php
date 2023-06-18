7c8b985f0b1451f3140061a10aee1f26eab94adf
Radvan Alexandru <rip777ton@gmail.com>
2023-06-18 20:10:54 +0300

    Add an avatar field in the user class

--- src/model/User.php
+++ src/model/User.php
@@ -4,13 +4,15 @@ class User implements JsonSerializable  {
     private $nickname;
     private $email;
     private $password_hash;
+    private $avatar;
 
    // function __constructor(){}
-    function __construct($id,$nickname,$email,$password_hash="clasified data"){
+    function __construct($id,$nickname,$email,$password_hash="clasified data",$avatar=""){
         $this->id=$id;
         $this->nickname=$nickname;
         $this->email=$email;
         $this->password_hash=$password_hash;
+        $this->avatar=$avatar;
     }
       function set_nickname($nickname){
         $this->nickname=$nickname;
@@ -36,12 +38,19 @@ class User implements JsonSerializable  {
       function get_password_hash(){
         return $this->password_hash;
       }
+      function set_avatar($avatar){
+        $this->avatar=$avatar;
+      }
+      function get_avatar(){
+        return $this->avatar;
+      }
       public function jsonSerialize() {
         return [
              "id" => $this->id,
             'nickname' => $this->nickname,
             'email' => $this->email,
-            'password_hash' => $this->password_hash
+            'password_hash' => $this->password_hash,
+            'avatar'=>$this->avatar
         ];
     }
 }
